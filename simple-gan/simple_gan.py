ed7c9c00523d111e189a62f61658c2d93652c4e1
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-07 16:36:12 +0100

    Added the generator class for a simple gan network

--- simple-gan/simple_gan.py
+++ simple-gan/simple_gan.py
@@ -18,10 +18,10 @@ fully connected feedforward network
 """
 
 class Discriminaor(nn.Module):
-    def __init__(self, in_features):        # input features size will be 784
+    def __init__(self, imageDimention):        # input features size will be 784
         super().__init__()
         self.discriminaor = nn.Sequential(
-            nn.Linear(in_features, 128),
+            nn.Linear(imageDimention, 128),
             nn.LeakyReLU(0.1),              # 0.1 is an arbitrary hyperparam
             nn.Linear(128, 1),              
             # This network is binary so 1 output --> Real or fake
@@ -34,6 +34,23 @@ class Discriminaor(nn.Module):
 
 """
 We now look to define our Generator that will be tasked with generating 
-imitations of the input data
+imitations of the input data. This will take random nouse and use it to generate
+an image of the same dimentions and the dataset we are using
 """
 
+class Generator(nn.Module):
+    def __init__(self, zDimention, imageDimention):
+        # zDimiention is the dimention of the latent noise the generator uses
+        # to make randomized datasets
+
+        super().__init__()
+
+        self.generator = nn.Sequential(
+            nn.Linear(zDimention, 256),
+            nn.LeakyReLU(0.1),
+            nn.LeakyReLU(256, imageDimention),
+            nn.Tanh(),                  # make output pixel val between -1 and 1
+        )
+
+        def forward(self, x):
+            return self.generator(x)
