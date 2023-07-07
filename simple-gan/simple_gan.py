0209071ff5456f05189742301d0785e9a3e12081
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-07 17:51:48 +0100

    Added discriminatortrainer

--- simple-gan/simple_gan.py
+++ simple-gan/simple_gan.py
@@ -24,7 +24,7 @@ class Discriminaor(nn.Module):
             nn.Linear(imageDimention, 128),
             nn.LeakyReLU(0.1),              # 0.1 is an arbitrary hyperparam
             nn.Linear(128, 1),              
-            # This network is binary so 1 output --> Real or fake
+            ### This network is binary so 1 output --> Real or fake
 
             nn.Sigmoid(),                   # So we get probabilities as output
         )
@@ -87,9 +87,37 @@ optimiserDiscriminator = optim.Adam(discriminator.parameters(), lr=learningRate)
 criterion = nn.BCELoss()
 
 
-# For Tensor board
+### For Tensor board
 writerFake = SummaryWriter(f"runs/GAN_MNIST/fake") # output fake images
 writerReal = SummaryWriter(f"runs/GAN_MNIST/real")
 step = 0
 
+"""
+We now write the model training loop
+"""
 
+for epoch in range(nEpochs):
+    for batchIdx, (realImages, _) in enumerate(loader):
+        realImages = realImages.view(-1, 28*28).to       # resize images
+        batchSize = realImages.shape[0]
+
+        ### Train discriminator : max log(D(realImages)) + log(1- D( G(z) ))
+        noise = torch.rand(batchSize, zDimention).to(device)    # rand noise tens
+        fake_image = generator(noise)                       # G(z)
+
+        discriminatorReal = discriminator(realImages).view(-1)    # D(realImage)
+        lossDiscriminatorReal = criterion(discriminatorReal,
+                                          torch.ones_like(discriminatorReal))
+
+        discriminatorFake = discriminator(fake).view(-1)          # D( G(z) )
+        lossDiscriminatorFake = criterion(discriminatorFake,
+                                          torch.zeros_like(discriminatorFake))
+
+        lossDiscriminator = (lossDiscriminatorReal + lossDiscriminatorFake)/2
+
+        discriminator.zero_grad()
+        lossDiscriminator.backward()
+        optimiserDiscriminator.step()
+
+
+        ### Train Generator
