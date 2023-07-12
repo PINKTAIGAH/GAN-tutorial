2d02bba174f89cb2054ad45f7c9195366861dc0b
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-13 00:37:23 +0100

    added encoding network for generator

--- pix2pix/generator.py
+++ pix2pix/generator.py
@@ -1,5 +1,11 @@
 import torch
 import torch.nn as nn
+from torch.nn.modules import activation
+
+"""
+Write generic convolution block used for the upscaling and downscaling of the 
+encoder/decoder sections
+"""
 
 class Block(nn.Module):
     
@@ -26,5 +32,35 @@ class Block(nn.Module):
         x = self.convolution(x)
         return self.dropout(x) if self.useDropout else x
 
-class Generator(nn.Module):
+"""
+Define the generator class
+"""
 
+class Generator(nn.Module):
+    def __init__(self, inChannels, features=64):
+        super().__init__()
+        self.initialDown = nn.Sequential(
+            nn.Conv2d(inChannels, features, kernel_size=4, stride=2, 
+                      padding=1, padding_mode="reflect"),
+            nn.LeakyReLU(),
+        )   # 128
+
+        self.down1 = Block(features, features*2, down=True,
+                           activation="leaky", useDropout=False)    # 64
+        self.down2 = Block(features*4, features*8, down=True,
+                           activation="leaky", useDropout=False)    # 32
+        self.down3 = Block(features*8, features*8, down=True,
+                           activation="leaky", useDropout=False)    # 16
+        self.down4 = Block(features*8, features*8, down=True,
+                           activation="leaky", useDropout=False)    # 8
+        self.down5 = Block(features*8, features*8, down=True,
+                           activation="leaky", useDropout=False)    # 5
+        self.down6 = Block(features*8, features*8, down=True,
+                           activation="leaky", useDropout=False)    # 2
+
+        # Final layer of the encoder
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(features*8, features*8, kernel_size=4, stride=2, 
+                      padding=1, padding_mode="reflect"),
+            nn.ReLU(),
+        )
